2189e5442ac612bc379bab66fc9bf3da15f5abf2
Bilal Karim <beekay-@users.noreply.github.com>
2018-04-08 16:28:45 -0600

    Added new registration date

--- js/site.js
+++ js/site.js
@@ -39,29 +39,29 @@ $(document).ready(function() {
         });
     }
 
-    // // Set the date we're counting down to
-    // var countDownDate = new Date("Feb 17, 2018 23:59:59").getTime();
-    //
-    // // Update the count down every 1 second
-    // var x = setInterval(function() {
-    //     // Get todays date and time
-    //     var now = new Date().getTime();
-    //     // Find the distance between now an the count down date
-    //     var distance = countDownDate - now;
-    //     // Time calculations for days, hours, minutes and seconds
-    //     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    //     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    //     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    //     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    //     // Display the result in the element with id="demo"
-    //     document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
-    //         minutes + "m " + seconds + "s ";
-    //     // If the count down is finished, write some text
-    //     if (distance < 0) {
-    //         clearInterval(x);
-    //         document.getElementById("demo").innerHTML = "";
-    //     }
-    // }, 1000);
+    // Set the date we're counting down to
+    var countDownDate = new Date("Apr 15, 2018 23:59:59").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+        // Get todays date and time
+        var now = new Date().getTime();
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Display the result in the element with id="demo"
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
+            minutes + "m " + seconds + "s ";
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "";
+        }
+    }, 1000);
 
     // Modal
     var overlay = document.createElement('div');
